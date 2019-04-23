2fd49ae26cfc9ee8a4fd3d1401c70a051bdb5cd1
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-23 20:37:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,11 +11,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {railroads: {}}
-	monopoly[:pieces] = 4
-	monopoly[:names] = {}
-	monopoly[:rent_in_dollars] = {}
-	monopoly
+	monopoly = {railroads: {pieces: 4, names: {},rent_in_dollars: {}}}
 end
 
 def monopoly_with_third_tier
